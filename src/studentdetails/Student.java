d62c3d7f97f196581d07c1f03869fddaa0e1a097
Jonathan Marsh <marsjona@sheridancollege.ca>
2025-05-13 17:01:38 -0400

    initial setup of student version 1

---
+++ src/studentdetails/Student.java
@@ -0,0 +1,44 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package studentdetails;
+
+/**
+ *
+ * @author john_m92
+ */
+public class Student {
+
+    /**
+     * @return the name
+     */
+    public String getName() {
+        return name;
+    }
+
+    /**
+     * @param name the name to set
+     */
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    /**
+     * @return the sid
+     */
+    public int getSid() {
+        return sid;
+    }
+
+    /**
+     * @param sid the sid to set
+     */
+    public void setSid(int sid) {
+        this.sid = sid;
+    }
+    
+    private String name;
+    private int sid;
+    
+}
---
+++ src/studentdetails/StudentDetails.java
@@ -0,0 +1,38 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package studentdetails;
+
+/**
+ *
+ * @author john_m92
+ */
+public class StudentDetails {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        
+        Student s1 = new Student();
+        s1.setName("peter");
+        s1.setSid(1);
+        Student s2 = new Student();
+        s2.setName("miko");
+        s2.setSid(2);
+        Student s3 = new Student();
+        s3.setName("king");
+        s3.setSid(3);
+        Student[] list = new Student[3]; //array of object
+        // TODO code application logic here
+        list[0]=s1;
+        list[1]=s2;
+        list[2]=s3;
+        for(int i=0;i<list.length;i++)
+        {
+            System.out.println(list[i].getName()+" "+list[i].getSid());
+        }
+    }
+    
+}
